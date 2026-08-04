4bdc9c41fb4995d191653fc3b3088fa5a203c4d5
imranparuk <imranparuk@live.com>
2026-08-04 21:21:56 +0200

    client: convert relayBox.test.js to TypeScript

--- client/src/__tests__/relayBox.test.ts
+++ client/src/__tests__/relayBox.test.ts
@@ -18,12 +18,20 @@ import {
   openRelayBlob,
   bytesToB64,
   b64ToBytes,
+  type BoxKeyPair,
 } from '../relayBox.js'
 
-const enc = (s) => new TextEncoder().encode(s)
-const dec = (b) => new TextDecoder().decode(b)
+const enc = (s: string) => new TextEncoder().encode(s)
+const dec = (b: Uint8Array) => new TextDecoder().decode(b)
 
-function seal(senderKP, recipientKP, { from = 'alice', to = 'bob', session = 'sess-1', data = 'secret' } = {}) {
+interface SealOpts {
+  from?: string
+  to?: string
+  session?: string
+  data?: unknown
+}
+
+function seal(senderKP: BoxKeyPair, recipientKP: BoxKeyPair, { from = 'alice', to = 'bob', session = 'sess-1', data = 'secret' }: SealOpts = {}) {
   return sealRelayBlob({
     plaintext: enc(JSON.stringify({ session, data })),
     senderBoxPriv: senderKP.privateKey,
